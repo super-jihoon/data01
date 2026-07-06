70df69b4e55ea09d293aea04b99028ac2cb0dcf5
super-jihoon <dkswlgns0483@gmail.com>
2026-07-06 19:29:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import streamlit as st
-st.title('여행취향')
-st.write('##친구와의 여행)
+import pandas as pd
+
+# 1. 페이지 기본 설정
+st.set_page_config(
+    page_title="나의 여행 취향 분석기",
+    page_icon="✈️",
+    layout="centered"
+)
