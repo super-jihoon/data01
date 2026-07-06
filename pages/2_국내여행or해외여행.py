f0b40442b1470e9a0ab12e69bb645c14967e5b1c
super-jihoon <dkswlgns0483@gmail.com>
2026-07-06 19:54:01 +0900

    Update 2_국내여행or해외여행.py

--- pages/2_국내여행or해외여행.py
+++ pages/2_국내여행or해외여행.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.header('드롭다운')
 
 option = st.selectbox(
-     '원하는 여행은 무엇인가요?',
+     '선호하는 여행 장소는 어디인가요?',
      ('국내 여행', '해외 여행'))
 
 st.write('당신이 선호하는 여행 장소는? ', option)
